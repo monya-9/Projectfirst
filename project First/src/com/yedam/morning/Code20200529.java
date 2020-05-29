76f88e83da10035d32ee821256ac5f60fdbf11ef
monya-9 <lee99714@naver.com>
2020-05-29 13:26:00 +0900

    야구게임 (번호중복 미해결)

---
+++ project First/src/com/yedam/morning/Code20200529.java
@@ -0,0 +1,50 @@
+package com.yedam.morning;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Code20200529 {
+	public static void main(String[] args) {
+		int[] selectNumber = new int[3];
+		int[] winningNumber = new int[3];
+		int[] scores = null;
+		Scanner scn = new Scanner(System.in);
+
+		System.out.println("게임시작.");
+		Random random = new Random();
+		System.out.println("번호가 생성되었습니다.");
+		for (int i = 0; i < 3; i++) {
+		selectNumber[i] = random.nextInt(9) + 1;
+		
+		}
+
+		while (true) {
+			int strake = 0;
+			int boll = 0;
+			System.out.print("첫번째 번호 입력: ");
+			winningNumber[0] = scn.nextInt();
+			System.out.print("두번째 번호 입력: ");
+			winningNumber[1] = scn.nextInt();
+			System.out.print("세번째 번호 입력: ");
+			winningNumber[2] = scn.nextInt();
+
+			for (int i = 0; i < 3; i++) {
+
+				for (int j = 0; j < 3; j++) {
+					if (selectNumber[i] == winningNumber[j]) {
+						if (i == j)
+							strake++;
+						else
+							boll++;
+
+					}
+				}
+			}
+			System.out.println("strake : " + strake + " boll: " + boll);
+			if (strake == 3)
+				break;
+		}
+		System.out.println("게임이 끝났습니다.");
+
+	}
+}
